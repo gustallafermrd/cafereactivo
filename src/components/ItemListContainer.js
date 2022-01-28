17266375b7214718860a93d29cf8b0db3c18238d
gustallafermrd <36135271+gustallafermrd@users.noreply.github.com>
2022-01-27 23:55:57 -0300

    desafio4
    Desafio cuatro

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+const ItemListContainer = (props) =>{
+  return(
+    <>
+      <div className="max-w-4xl mx-auto px-2">
+        <p>Este es el ItemListContainer {props.contenido}</p>
+      </div>
+    </>
+  );
+}
+export default ItemListContainer;
