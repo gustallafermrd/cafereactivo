2cb51e9dd17823fd31ae7fa50965abc3184b78ee
gustallafermrd <36135271+gustallafermrd@users.noreply.github.com>
2022-03-02 07:36:39 -0300

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,27 +6,43 @@ const Cart =() => {
   const prueba = useContext(CartContext);
   return (
     <>
-      <h1>Tu carrito!</h1>
-      <Link to='/'>Seguir Comprando</Link>
-      {
-        (prueba.cartList.length > 0)
-        ? <button onClick={prueba.removeList}>Borrar todos los Productos</button>
-        : <h2>Tu Carrito esta Vacio</h2>
-      }
+      <div className="w-full text-center">
+        <h1 className="text-xl title-font tracking-widest">Tu carrito!</h1>
+      </div>
+      <div className="flex flex-wrap text-center m-4">
+        <Link to='/'><button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Ve a Comprar</button></Link>
+        {
+          (prueba.cartList.length > 0)
+          ? <button onClick={prueba.removeList} className="flex ml-auto text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-700 rounded">Borrar todos los Productos</button>
+          : <h2>Tu Carrito esta Vacio</h2>
+        }
+      </div>
 
-      {
+      <div className="w-full h-full">
+      {  
         prueba.cartList.length > 0 &&
           prueba.cartList.map(item => (
             <>
-              <p>Producto: {item.name}</p>
-              <p>Precio: {item.price}</p>
-              <p>Cantidad: {item.qtyItem}</p>
-              <img src={item.image} alt={item.name}/>
-              <button onClick={prueba.deleteProd}>Eliminar</button>
-              <p>Total: ${prueba.totalCart()}</p>
+              <section className="text-gray-600 body-font overflow-hidden">
+                <div className="container px-4 py-4 mx-auto">
+                  <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                      <img className="lg:w-1/2 w-full lg:h-auto h-12 object-cover object-center rounded" src={item.image} alt={item.name}/>
+                      <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                        <h2 className="text-xl title-font tracking-widest">Nombre: {item.name}</h2>
+                        <h3 className="text-md title-font tracking-widest">Precio: {item.price}</h3>
+                        <h4 className="text-md title-font tracking-widest">Cantidad: {item.qtyItem}</h4>
+                        <p className="text-md title-font tracking-widest">Total: ${prueba.totalCart()}</p>
+                        <button onClick={prueba.deleteProd} className="flex text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-700 rounded">Eliminar</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
             </>
           ))
       }
+      </div>
     </>
   )
 }
